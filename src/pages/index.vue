41962be28da8e4852736dabf92221e378aa9093b
huanshun <569259131@qq.com>
2022-11-01 16:14:47 +0800

    feat:update

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,28 +1,39 @@
 <script setup lang="ts">
 
 interface GameLog {
-  row:number,
-  col:number,
+  row: number,
+  col: number,
 }
 
 const gameLogs = ref<GameLog[]>([])
 
-function onClick(row:number,col:number) {
+function onClick(row: number, col: number) {
   gameLogs.value.push({
-    row,col
+    row, col
   })
 
-  console.log('gameLogs :>> ', gameLogs);
+  let player1: GameLog[] = []
+  let player2: GameLog[] = []
+  gameLogs.value.forEach((item, index) => {
+    if (index % 2 === 0) {
+      player1.push(item)
+    } else {
+      player2.push(item)
+    }
+  })
+
+  console.log('player1 :>> ', player1);
+  console.log('player2 :>> ', player2);
 }
 
-function pieces(row:number,col:number) {
+function pieces(row: number, col: number) {
 
-  const index = gameLogs.value.findIndex(item=>(item.row===row&&item.col===col))
-  if (index===-1) {
+  const index = gameLogs.value.findIndex(item => (item.row === row && item.col === col))
+  if (index === -1) {
     return ''
   }
 
-  if (index%2===0) {
+  if (index % 2 === 0) {
     return 'i-carbon:radio-button'
   }
   // 'i-carbon:radio-button'
@@ -35,8 +46,9 @@ function pieces(row:number,col:number) {
   <div flex="~" justify-center>
     <div>
       <div v-for="row in 3" flex="~">
-        <div v-for="col in 3" w-20 h-20 border-1 text-5xl flex="~" justify-center items-center @click="onClick(row,col)">
-          <div :class="pieces(row,col)"></div>
+        <div v-for="col in 3" w-20 h-20 border-1 text-5xl flex="~" justify-center items-center
+          @click="onClick(row, col)">
+          <div :class="pieces(row, col)"></div>
         </div>
       </div>
     </div>
